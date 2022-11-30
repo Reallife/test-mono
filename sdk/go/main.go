0219c73f7660a7a2a221d85cf52af8fc69e40a50
Sergey Ryabov <ryaboff.s.a@gmail.com>
2022-11-30 14:36:41 +0500

    change sdk

--- sdk/go/main.go
+++ sdk/go/main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"context"
-	"fmt"
 )
 
 func main() {
 	l := &Loader{}
 
 	d, err := l.GetDict(context.Background(), DictDir1, "v0.0.1")
-	
+
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v", d)
+	println(d.Meta.Description)
 }
